6e4eb3d9f6f5fe0d8658f73e91642e1e55d62f26
mdamirulislam313 <mdmamirllmama@gmail.com>
2025-08-01 02:05:21 +0600

    added swirching option

---
+++ js/features.js
@@ -0,0 +1,16 @@
+// show the cash uot form
+document.getElementById('btn-show-cash-out')
+.addEventListener('click', function(){
+    document.getElementById('cash-out-form').classList.remove('hidden');
+    // hidden the add money form
+    document.getElementById('add-money-form')
+    .classList.add('hidden')
+})
+
+
+// show add money form and hide the cash out form
+document.getElementById ('btn-show-add-money')
+.addEventListener('click',function(){
+    document.getElementById('add-money-form').classList.remove ('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden')
+})
